a83c6dea23647c79d41dc48853d40ba7be2dca84
Lev <name89213126414@gmail.com>
2026-02-02 16:25:58 +0300

    Тёмный цвет иконок

--- app/src/main/java/com/leonardower/mymovie/ui/theme/Theme.kt
+++ app/src/main/java/com/leonardower/mymovie/ui/theme/Theme.kt
@@ -3,7 +3,9 @@ package com.leonardower.mymovie.ui.theme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorScheme = darkColorScheme(
     background = DarkBg,
@@ -21,9 +23,29 @@ fun MyMovieTheme(
 ) {
     val colorScheme = DarkColorScheme
 
+    SetupSystemUI()
+
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
         content = content
     )
+}
+
+@Composable
+fun SetupSystemUI() {
+    val systemUiController = rememberSystemUiController()
+
+    SideEffect {
+        systemUiController.setSystemBarsColor(
+            color = Color.Transparent,
+            darkIcons = false
+        )
+
+        systemUiController.setNavigationBarColor(
+            color = DarkBg,
+            darkIcons = false,
+            navigationBarContrastEnforced = false
+        )
+    }
 }
