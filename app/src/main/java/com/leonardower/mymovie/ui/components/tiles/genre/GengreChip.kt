e04111d6a7dc285085fb2fecb34b70aa0be9dde1
Lev <name89213126414@gmail.com>
2026-03-25 10:23:40 +0300

    UI

--- app/src/main/java/com/leonardower/mymovie/ui/components/tiles/genre/GengreChip.kt
+++ app/src/main/java/com/leonardower/mymovie/ui/components/tiles/genre/GengreChip.kt
@@ -2,7 +2,9 @@ package com.leonardower.mymovie.ui.components.tiles.genre
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
@@ -10,9 +12,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.leonardower.mymovie.data.local.entities.Genre
-import com.leonardower.mymovie.ui.theme.GrayBg
+import com.leonardower.mymovie.ui.theme.GrayButtonColor
+import com.leonardower.mymovie.ui.theme.MyMovieTheme
 
 @Composable
 fun GenreChip(
@@ -23,7 +27,7 @@ fun GenreChip(
     Box(
         modifier = modifier
             .wrapContentSize()
-            .background(GrayBg)
+            .background(GrayButtonColor)
             .padding(horizontal = 30.dp, vertical = 12.dp)
             .clickable { onClick() },
     ) {
@@ -46,4 +50,16 @@ fun GenreChip(
         modifier = modifier,
         onClick = onClick
     )
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    MyMovieTheme {
+        Row(horizontalArrangement = Arrangement.spacedBy(12.dp)) {
+            repeat(3) {index ->
+                GenreChip("Genre $index")
+            }
+        }
+    }
 }
